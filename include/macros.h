1fc20e56311a423031c1bfce4998345eb50a9d5d
brunodea <brunodea@inf.ufsm.br>
2011-10-08 16:06:06 -0300

    main and macros added.

---
+++ include/macros.h
@@ -0,0 +1,11 @@
+#ifndef _CGA_T2_MACROS_H_
+#define _CGA_T2_MACROS_H_
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
+
+
+
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,61 @@
+/*
+    Bruno Romero de Azevedo
+    2910098
+ */
+#include "glfw.h"
+#include <cstdlib>
+
+#include "macros.h"
+
+/*
+ * Callback functions
+ */
+void GLFWCALL keyEventCallback(int key, int state)
+{
+}
+
+void GLFWCALL handleResize(int width, int height)
+{
+    glViewport(0, 0, width, height);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    
+    gluPerspective(45.f,(float)width/(float)height,0.1,5000);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
+
+void setCallBacks()
+{
+    glfwSetKeyCallback(keyEventCallback);
+    glfwSetWindowSizeCallback(handleResize);
+}
+
+void initOpenGL()
+{
+    handleResize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    glEnable(GL_DEPTH_TEST);
+    glClearColor(0.f, 0.f, 0.f, 1.f);
+}
+
+int main()
+{
+    if(!glfwInit())
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    if(!glfwOpenWindow(WINDOW_WIDTH, WINDOW_HEIGHT, 8, 8, 8, 8, 8, 8, GLFW_WINDOW))
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+    initOpenGL();
+    setCallBacks();
+
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
+}
+
